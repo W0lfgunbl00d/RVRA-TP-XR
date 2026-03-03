1a18328c24292036bf0d3eed14f2f54bbfd1b12e
W0lfgunbl00d <elakrabasouissi@gmail.com>
2026-03-03 15:02:46 +0100

    Grabbable handle

---
+++ Assets/Scripts/Controllers/GrabSystemHandle.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class GrabSystemHandle : XRGrabInteractable
+{
+    [Header("Parent Transform Override")]
+    [Tooltip("Assign the parent object that should move instead of this object")]
+    public Transform targetParent;
+
+    private Vector3 localPositionOffset;
+    private Quaternion localRotationOffset;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        movementType = MovementType.Instantaneous;
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+
+        if (targetParent == null) return;
+
+        // Cast to IXRInteractor (XRI 3.x)
+        IXRSelectInteractor interactor = args.interactorObject;
+        Transform interactorTransform = interactor.GetAttachTransform(this);
+
+        localPositionOffset = targetParent.InverseTransformPoint(interactorTransform.position);
+        localRotationOffset = Quaternion.Inverse(interactorTransform.rotation) * targetParent.rotation;
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+
+        if (!isSelected || targetParent == null) return;
+
+        if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+        {
+            IXRSelectInteractor interactor = firstInteractorSelecting;
+            Transform interactorTransform = interactor.GetAttachTransform(this);
+
+            targetParent.position = interactorTransform.TransformPoint(-localPositionOffset);
+            targetParent.rotation = interactorTransform.rotation * localRotationOffset;
+        }
+    }
+}
